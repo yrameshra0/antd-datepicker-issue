77d13a8cbc2834609bc21142ce1b8d61728e029a
Y Ramesh Rao <ryeduvakula@paypal.com>
2019-07-08 10:12:32 +0530

    AntD issue related commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Select, DatePicker } from 'antd'
+const Option = Select.Option;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Select style={{ width: 220 }} >
+          <Option key="startDate"> <DatePicker /> </Option>
+          <Option key="middleDate"> <DatePicker /> </Option>
+          <Option key="endDate"> <DatePicker/> </Option>
+        </Select>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { version } from "antd";
+import "antd/dist/antd.css";
+import "./index.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <div className="App">
+        <h1>Please fork this codesandbox to reproduce your issue.</h1>
+        <div>Current antd version: {version}</div>
+        <div style={{ marginTop: "16px" }}>
+            <App />
+        </div>
+    </div>,
+    document.getElementById("root")
+);
